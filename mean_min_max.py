77270346666e3c5ab1df71e5e5750cd52b9a4d7a
Michele Bottazzi <michele.bottazzi@gmail.com>
2018-11-15 10:58:20 +0100

    Path changed

--- mean_min_max.py
+++ mean_min_max.py
@@ -2,7 +2,7 @@ import numpy
 import matplotlib.pyplot
 #provide data
 
-data = numpy.loadtxt(fname='inflammation-01.csv', delimiter=',')
+data = numpy.loadtxt(fname='data/inflammation-01.csv', delimiter=',')
 
 fig = matplotlib.pyplot.figure(figsize=(10.0, 5.0))
 
